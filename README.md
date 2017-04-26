5fe025ecfd6319e0de33f80a05fcb9619381afb6
keellyp <kelly.phan@hetic.net>
2017-04-26 23:01:27 +0200

    :tada: Initial commit
    Signed-off-by: keellyp <kelly.phan@hetic.net>

---
+++ README.md
@@ -0,0 +1,51 @@
+# static-template
+A static template for HTML5, CSS3, JS run with Gulp
+
+
+## Prerequisites
+You need to install [Node.js](https://nodejs.org/en/)
+
+
+### Install
+Download the repository then go to the folder builder in your terminal.
+
+```
+cd .../static-template/builder
+```
+
+Once you are in there, you have to install node-modules
+
+```
+npm install
+```
+*You may need to install with sudo if you are a macOS user*
+
+
+
+## Compatibily
+* Gulp.js
+* Javascript
+* HTML5 / CSS3
+
+
+
+## Features
+* Sourcemaps
+* Watchers
+* Notifications
+
+
+### Tasks
+* "style" allows to compile the SASS into CSS, autoprefix it, minified and replace into the dist folder.
+* "javascript" concatenates all the javascript files into one, minified it and replace into the dist folder
+* "images" minifies all the images and place it on dist folder.
+* "fonts" moves all the fonts into dist folder
+* "watch" watches the changes in your files
+
+
+## Author
+[Kelly Phan](http://kellyphan.fr)
+
+
+## Licence
+This repository is under MIT Licence.
---
+++ builder/gulpfile.js
@@ -0,0 +1,95 @@
+// Variables
+const gulp                = require( 'gulp' );
+const config =
+{
+    scss:   "../assets/scss/",
+    js:     "../assets/javascript/",
+    assets: "../assets/",
+    dist:   "../dist/"
+};
+
+// Tools dependencies
+gulp_rename         = require( 'gulp-rename' ),
+gulp_plumber        = require( 'gulp-plumber' ),
+gulp_notify         = require( 'gulp-notify' ),
+gulp_sourcemaps     = require( 'gulp-sourcemaps' ),
+
+// Image depedency
+gulp_imagemin       = require( 'gulp-imagemin' );
+
+// Style dependencies
+gulp_sass           = require( 'gulp-sass' ),
+gulp_autoprefixer   = require( 'gulp-autoprefixer' ),
+gulp_cssnano        = require( 'gulp-cssnano' ),
+
+// Javascript dependencies
+gulp_concat         = require( 'gulp-concat' );
+gulp_uglify         = require( 'gulp-uglify' );
+
+
+// Default task
+gulp.task( 'default', ['watch'], function(){} );
+
+// Compile SCSS into CSS, create a sourcemaps, autoprefix the code and put the file into dist folder
+gulp.task( 'style', function()
+{
+    return gulp.src(config.scss + 'main.scss')
+        .pipe( gulp_plumber(
+            { errorHandler: gulp_notify.onError('SASS Erro <%= error.message %>') }
+        ) )
+        .pipe( gulp_sourcemaps.init() )
+        .pipe( gulp_sass(
+            { outputStyle: 'compressed' }).on('error', gulp_sass.logError) )
+        .pipe( gulp_autoprefixer(
+            {
+                browsers: ['last 2 versions'],
+                cascade: false
+            }
+        ) )
+        .pipe( gulp_cssnano() )
+        .pipe( gulp_sourcemaps.write() )
+        .pipe( gulp_rename('style.min.css') )
+        .pipe( gulp.dest(config.dist + 'css') )
+        .pipe( gulp_notify("style is cool !") )
+} );
+
+
+gulp.task( 'javascript', function()
+{
+    return gulp.src( config.js + '*.js' )
+        .pipe( gulp_plumber(
+            { errorHandler: gulp_notify.onError('JS Erro <%= error.message %>') }
+        ) )
+        .pipe( gulp_sourcemaps.init() )
+        .pipe( gulp_concat('script.js') )
+        .pipe( gulp_uglify() )
+        .pipe( gulp_sourcemaps.write() )
+        .pipe( gulp_rename('script.min.js') )
+        .pipe( gulp.dest(config.dist + 'js') )
+        .pipe( gulp_notify("js is cool !") )
+} );
+
+// Minifies images
+gulp.task( 'images', function()
+{
+    return gulp.src(config.assets + 'images/*')
+        .pipe( gulp_imagemin() )
+        .pipe( gulp.dest(config.dist + 'img') )
+        .pipe( gulp_notify('minified !') )
+} );
+
+// Replace font into dist folder
+gulp.task( 'fonts', function()
+{
+    return gulp.src(config.assets + 'fonts/*')
+        .pipe( gulp.dest(config.dist + 'fonts') )
+} );
+
+// Watch all my task
+gulp.task( 'watch', ['style', 'javascript', 'images', 'fonts'], function()
+{
+    gulp.watch(config.scss + '**/*.scss', ['style']);
+    gulp.watch(config.js + '**/*.js', ['javascript']);
+    gulp.watch(config.assets + 'images/*', ['images']);
+    gulp.watch(config.assets + 'fonts/*', ['fonts']);
+} );
